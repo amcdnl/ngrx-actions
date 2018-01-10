3c7e242ec9eb4d8e25dd95b35e1666aaf0be6d6c
sandangel <vinhsannguyen91@gmail.com>
2018-01-11 00:29:47 +0900

    refactor(ofAction): use filter function like ngrx/effect ofType

--- src/of-action.ts
+++ src/of-action.ts
@@ -1,15 +1,18 @@
 import { Action } from '@ngrx/store';
-import { filter } from 'rxjs/operators';
-import { OperatorFunction } from 'rxjs/interfaces';
+import { Actions } from '@ngrx/effects';
+import { filter } from 'rxjs/operator/filter';
+import { OperatorFunction, MonoTypeOperatorFunction } from 'rxjs/interfaces';
 
 import { ActionType } from './symbols';
 
-export function ofAction<T extends Action>(allowedType: ActionType<T>): OperatorFunction<Action, T>;
+export function ofAction<T extends Action>(allowedType: ActionType<T>): MonoTypeOperatorFunction<T>;
 export function ofAction<T extends Action>(...allowedTypes: ActionType[]): OperatorFunction<Action, T>;
-export function ofAction(...allowedTypes: ActionType[]): OperatorFunction<Action, Action> {
+export function ofAction<T extends Action>(...allowedTypes: ActionType[]): OperatorFunction<Action, T> {
   const allowedMap: { [type: string]: boolean } = {};
   allowedTypes.forEach(klass => (allowedMap[new klass().type] = true));
-  return filter((action: Action) => {
-    return allowedMap[action.type];
-  });
+  return function(source$: Actions<T>): Actions<T> {
+    return filter.call(source$, (action: Action) => {
+      return allowedMap[action.type];
+    });
+  };
 }
