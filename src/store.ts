e5432dc1aba0046429197d68709f100145797283
sandangel <vinhsannguyen91@gmail.com>
2018-01-11 18:34:29 +0900

    revert: switch back to any type for initialState

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,8 @@
 import { INITIAL_STATE_KEY } from './keys';
 
-export function Store<TState extends object>(initialState?: TState): (target: Function) => void;
-export function Store(initialState?: object): (target: Function) => void;
-export function Store(initialState: object = {}) {
+export function Store<TState>(initialState?: TState): (target: Function) => void;
+export function Store(initialState?: any): (target: Function) => void;
+export function Store(initialState: any = {}) {
   return function(target: Function) {
     Reflect.defineMetadata(INITIAL_STATE_KEY, initialState, target);
   };
